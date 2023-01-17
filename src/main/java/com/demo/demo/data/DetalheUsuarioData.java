cdc391ced39f994def0bf3286a4d5f3e1dcaa7f3
rafabrasail <rafaelrosenberg89@gmail.com>
2023-01-17 16:42:43 -0300

    mudanças para configurar o jwt antes do pacore jwt

---
+++ src/main/java/com/demo/demo/data/DetalheUsuarioData.java
@@ -0,0 +1,54 @@
+package com.demo.demo.data;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.demo.demo.model.UsuarioModel;
+
+public class DetalheUsuarioData implements UserDetails {
+    
+    private final Optional<UsuarioModel> usuario;
+
+    public DetalheUsuarioData(Optional<UsuarioModel> usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return usuario.orElse(new UsuarioModel()).getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario.orElse(new UsuarioModel()).getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/demo/demo/service/DetalheUsuarioServiceImpl.java
@@ -0,0 +1,31 @@
+package com.demo.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.demo.demo.data.DetalheUsuarioData;
+import com.demo.demo.model.UsuarioModel;
+import com.demo.demo.repository.UsuarioRepository;
+
+@Component
+public class DetalheUsuarioServiceImpl implements UserDetailsService {
+    
+    private final UsuarioRepository repository;
+
+    public DetalheUsuarioServiceImpl(UsuarioRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        Optional<UsuarioModel> usuario = repository.findByLogin(username);
+        if(!usuario.isPresent()){
+            throw new UsernameNotFoundException("Usuário [" + username + "] não encontrado");
+        }
+        return new DetalheUsuarioData(usuario);
+    }
+}
